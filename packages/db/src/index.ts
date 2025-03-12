f97737b0fb3b2864706937aaded74a0c66d8514e
sameer2748 <mrao27488@gmail.com>
2025-03-12 23:34:46 +0530

    starting implement fast position sharing using redis

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+export default new PrismaClient({
+  datasources: {
+    db: {
+      url:
+        process.env.DATABASE_URL +
+        "&connection_limit=1&idle_in_transaction_session_timeout=60000",
+    },
+  },
+});
